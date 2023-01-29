630918a7449a5d50fdcdd9776e36bf934b77f5bb
vv1ll14n <williandsantosoliveira@gmail.com>
2023-01-29 14:19:42 -0300

    ver 1

--- qrcode.js
+++ qrcode.js
@@ -1,6 +1,6 @@
 function GerarQRCode(){
     var inputUsuario = document.querySelector('textarea').value;
-    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=150x150&chld=H&chl=';
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=315x315&chld=H&chl=';
     var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputUsuario);
     document.querySelector('#QRCodeImage').src = conteudoQRCode;
 }
